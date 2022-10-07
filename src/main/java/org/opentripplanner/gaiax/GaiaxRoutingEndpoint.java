8ac97717bb3edab83139f61a5e9a7d4c81432303
Timon Sachweh <timon@sachweh.de>
2022-10-07 13:14:50 +0200

    Adding get endpoint for routing

--- src/main/java/org/opentripplanner/gaiax/GaiaxRoutingEndpoint.java
+++ src/main/java/org/opentripplanner/gaiax/GaiaxRoutingEndpoint.java
@@ -1,7 +1,11 @@
 package org.opentripplanner.gaiax;
 
+import com.azure.core.annotation.Get;
+import com.azure.core.annotation.Post;
+import java.io.IOException;
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,6 +13,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.glassfish.grizzly.http.server.Request;
 import org.opentripplanner.gaiax.model.GaiaXStartEndRoutingRequest;
 import org.opentripplanner.gaiax.model.response.GaiaXRoutingResponse;
@@ -29,6 +34,10 @@ public class GaiaxRoutingEndpoint {
   @QueryParam("eta")
   private boolean eta;
 
+  @DefaultValue("")
+  @QueryParam("routingParams")
+  private String request;
+
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   public GaiaXRoutingResponse routing(
@@ -38,5 +47,23 @@ public class GaiaxRoutingEndpoint {
   ) {
     GaiaXRoutingService routingService = new GaiaXRoutingService(this.otpServer);
     return routingService.getRouteForFullRequest(request, routing, eta);
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public GaiaXRoutingResponse routingParameterized(
+    @Context UriInfo uriInfo,
+    @Context Request grizzlyRequest
+  ) throws IOException {
+    if (request.equals("")) {
+      return null;
+    }
+    ObjectMapper mapper = new ObjectMapper();
+    GaiaXStartEndRoutingRequest routingRequest = mapper.readValue(
+      request,
+      GaiaXStartEndRoutingRequest.class
+    );
+    GaiaXRoutingService routingService = new GaiaXRoutingService(this.otpServer);
+    return routingService.getRouteForFullRequest(routingRequest, routing, eta);
   }
 }
